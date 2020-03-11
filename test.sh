f6f6700a8d1230d087d9c9502592d0aa3100eca4
Jan Mojžíš <jan.mojzis@gmail.com>
2020-03-11 09:34:18 +0100

    .travis.yml test add

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# 20200311
+# Jan Mojzis
+# Public domain.
+
+# change directory to $AUTOPKGTEST_TMP
+cd "${AUTOPKGTEST_TMP}"
+
+./ntpserver.py -v 127.0.0.1 123 &
+ntpserverpid=$!
+
+cleanup() {
+  ex=$?
+  #kill ntpserver
+  kill -TERM "${ntpserverpid}" 1>/dev/null 2>/dev/null || :
+  kill -KILL "${ntpserverpid}" 1>/dev/null 2>/dev/null || :
+  exit "${ex}"
+}
+trap "cleanup" EXIT TERM INT
+
+sleep 5
+
+ntpdate -vdq 127.0.0.1 || exit 111
+
+exit 0
